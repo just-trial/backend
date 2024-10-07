4baeb6c26f3d6f172a11f4a8cbb58c932fa441c9
Davi A. Wasserberg <davi.abreu.w@gmail.com>
2024-10-07 15:52:16 -0300

    [Cors] demo disabled

--- lib/just_travel_web/endpoint.ex
+++ lib/just_travel_web/endpoint.ex
@@ -47,5 +47,6 @@ defmodule JustTravelWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug CORSPlug, origin: ["*"]
   plug JustTravelWeb.Router
 end
--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule JustTravel.MixProject do
       {:absinthe, "~> 1.6"},
       {:absinthe_plug, "~> 1.5"},
       {:scrivener_ecto, "~> 2.0"},
-      {:absinthe_error_payload, "~> 1.0"}
+      {:absinthe_error_payload, "~> 1.0"},
+      {:cors_plug, "~> 3.0"}
     ]
   end
 
